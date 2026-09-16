6a693046806fce366e5eb298e8426a0e3fa0bc39
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-09-16 17:18:39 +0700

    feat(spmb): expose verification status on SPMB page

--- src/routes/_protected._app.akademik.spmb.tsx
+++ src/routes/_protected._app.akademik.spmb.tsx
@@ -1,6 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import SPMB from "@/pages/akademik/SPMB";
+import { SpmbVerificationOverview } from "@/components/akademik/SpmbVerificationOverview";
+
+function SpmbPage() {
+  return (
+    <div className="space-y-6">
+      <SpmbVerificationOverview />
+      <SPMB />
+    </div>
+  );
+}
 
 export const Route = createFileRoute("/_protected/_app/akademik/spmb")({
-  component: SPMB,
+  component: SpmbPage,
 });
