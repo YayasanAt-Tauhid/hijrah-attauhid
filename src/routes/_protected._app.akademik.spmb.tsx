12a5b0bff261710722d4f33115b1ae9049b29fd5
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-09-25 07:45:38 +0700

    fix: render nested SPMB detail route

--- src/routes/_protected._app.akademik.spmb.tsx
+++ src/routes/_protected._app.akademik.spmb.tsx
@@ -1,16 +1,5 @@
-import { createFileRoute } from "@tanstack/react-router";
-import SPMB from "@/pages/akademik/SPMB";
-import { SpmbVerificationOverview } from "@/components/akademik/SpmbVerificationOverview";
-
-function SpmbPage() {
-  return (
-    <div className="space-y-6">
-      <SpmbVerificationOverview />
-      <SPMB />
-    </div>
-  );
-}
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected/_app/akademik/spmb")({
-  component: SpmbPage,
+  component: Outlet,
 });
